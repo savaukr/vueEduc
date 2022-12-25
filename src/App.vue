8026fa59b90032af799bcd1889e2aa0d0b499042
SlavaPachevsky <savaukr17101980@gmail.com>
2022-12-25 06:07:57 +0200

    add likes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Counts of likes: {{ likes }}</div>
+    <button @click="likes = likes + 1">Increase like</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    };
+  },
+};
+</script>
+<style></style>
